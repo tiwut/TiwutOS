38aa18fe5897d254f0c598ae94e79e83f447074b
Tiwut <145930346+tiwut@users.noreply.github.com>
2026-05-01 14:48:38 -0700

    Update build.sh

--- C&CPP/v1.5/build.sh
+++ C&CPP/v1.5/build.sh
@@ -7,15 +7,13 @@ rm -f *.o kernel.bin TiwutOS.iso
 echo "2. Compiling C++ Files..."
 CPPFLAGS="-m32 -ffreestanding -fno-rtti -fno-exceptions -Wall -Wextra -c"
 
-# We only compile kernel.cpp as it now contains all necessary logic (Utils, Screen, Keyboard)
-# This avoids ODR violations with old screen.cpp/keyboard.cpp
+
 g++ $CPPFLAGS kernel.cpp -o kernel.o
 
 echo "3. Assembling Bootloader..."
 nasm -f elf32 boot.asm -o boot.o
 
 echo "4. Linking..."
-# Only link boot.o and kernel.o
 ld -m elf_i386 -T link.ld -o kernel.bin boot.o kernel.o
 
 echo "5. Building ISO..."
